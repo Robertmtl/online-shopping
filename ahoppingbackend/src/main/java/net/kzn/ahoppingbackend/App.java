3ef081f554be96218a9a6f6ceb7d91c00ab94967
Robert Liu <robert_liu@live.ca>
2018-09-02 20:13:54 -0400

    My project setup completed successfully

---
+++ ahoppingbackend/src/main/java/net/kzn/ahoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.kzn.ahoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
